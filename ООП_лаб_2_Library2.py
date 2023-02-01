d4842aff58a085d53a743c9be0218106c34c76f1
konzhinadasha <119069958+konzhinadasha@users.noreply.github.com>
2023-02-01 19:09:22 +0300

    ООП_лаб_2_попытка-2

---
+++ ООП_лаб_2_Library2.py
@@ -0,0 +1,66 @@
+BOOKS_DATABASE = [
+    {
+        "id_": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id_": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f"Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})"
+
+
+class Library:
+
+    def __init__(self, books=None):
+        if books is None:
+            books = []
+        self.books = books
+
+    def get_next_book_id(self) -> int:
+        if len(self.books) == 0:
+            id_ = 1
+        else:
+            id_ = self.books[-1].id_ + 1
+        return id_
+
+    def get_index_by_book_id(self, index) -> int:
+        new_index = 0
+        for i, book in enumerate(self.books):
+            if int(i) + 1 == index:
+                new_index = i
+                break
+            else:
+                raise ValueError("Книги с запрашиваемым id не существует")
+        return new_index
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id_"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+#  пустая строка
